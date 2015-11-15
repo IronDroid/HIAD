6e261e20955131dfd14d15111c720bd4f367be06
Jhonny <jhonlimaster@gmail.com>
2015-11-14 23:04:19 -0400

    styles main activity

--- app/src/main/java/org/ajcm/hiad/MainActivity.java
+++ app/src/main/java/org/ajcm/hiad/MainActivity.java
@@ -6,15 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView numberHimno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        numberHimno = (TextView) findViewById(R.id.number_himno);
     }
 
     @Override
@@ -33,4 +38,51 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void numOk(View view) {
+        Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+    }
+
+    public void inputDelete(View view) {
+        numberHimno.setText("");
+    }
+
+    public void number0(View view) {
+        numberHimno.setText(numberHimno.getText() + "0");
+    }
+
+    public void number9(View view) {
+        numberHimno.setText(numberHimno.getText() + "9");
+    }
+
+    public void number8(View view) {
+        numberHimno.setText(numberHimno.getText() + "8");
+    }
+
+    public void number7(View view) {
+        numberHimno.setText(numberHimno.getText() + "7");
+    }
+
+    public void number6(View view) {
+        numberHimno.setText(numberHimno.getText() + "6");
+    }
+
+    public void number5(View view) {
+        numberHimno.setText(numberHimno.getText() + "5");
+    }
+
+    public void number4(View view) {
+        numberHimno.setText(numberHimno.getText() + "4");
+    }
+
+    public void number3(View view) {
+        numberHimno.setText(numberHimno.getText() + "3");
+    }
+
+    public void number2(View view) {
+        numberHimno.setText(numberHimno.getText() + "2");
+    }
+
+    public void number1(View view) {
+        numberHimno.setText(numberHimno.getText() + "1");
+    }
 }
